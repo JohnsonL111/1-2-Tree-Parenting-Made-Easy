fe154b01a47642b83e1dc8fde5e08063db9a5bd5
jpa143 <jonathan_patrick@sfu.ca>
2021-10-28 12:43:56 -0700

    fix timer error
    -fix error where the timer does not countdown
    -user no longer can change the option when the timer is running

--- app/src/main/java/cmpt276/as2/parentapp/UI/TimeoutActivity.java
+++ app/src/main/java/cmpt276/as2/parentapp/UI/TimeoutActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -20,7 +21,6 @@ import cmpt276.as2.parentapp.R;
 
 //timer button= start and stop button
 public class TimeoutActivity extends AppCompatActivity {
-    private static final String INITIAL_TIME = "Initial Time";
     private static final String DURATION_SETTING = "Duration Settings";
     private static final String DURATION_CHOICE = "Duration Choice";
 
@@ -28,41 +28,44 @@ public class TimeoutActivity extends AppCompatActivity {
     Button timerButton;
     Button resetButton;
     Button optionButton;
-    TextView timeout;
+    TextView timeoutText;
     boolean timerIsRunning;
     int initialTime;
     int timeLeft;
     CountDownTimer timer;
 
-    public static Intent makeIntent(Context context, int initialTime){
+    public static Intent makeIntent(Context context){
         Intent intent = new Intent(context, TimeoutActivity.class);
-        intent.putExtra(INITIAL_TIME,initialTime);
         return intent;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getDuration();
         setContentView(R.layout.activity_timeout);
         timerButton = findViewById(R.id.StartStopButton);
         resetButton = findViewById(R.id.ResetButton);
         optionButton=findViewById(R.id.OptionButton);
+        timeoutText=findViewById(R.id.timeoutText);
+
         timerButton.setText("START");
-        getDuration();
         timerButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 if(!timerIsRunning) {
-                    timerButton.setText("STOP");
-                    timerIsRunning=true;
                     startTimer();
+                    timerIsRunning=true;
+                    timerButton.setText("STOP");
+
 
                 }
                 else{
-                    timerButton.setText("START");
-                    timerIsRunning=false;
                     stopTimer();
+                    timerIsRunning=false;
+                    timerButton.setText("START");
+
                 }
             }
         });
@@ -70,56 +73,60 @@ public class TimeoutActivity extends AppCompatActivity {
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                timer.cancel();
                 timeLeft=initialTime;
+                updateTimer(timeLeft);
+                startTimer();
             }
         });
 
-        if(!timerIsRunning) {
-            optionButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
+        optionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!timerIsRunning){
                     Intent i = TimeoutOptionActivity.makeIntent(TimeoutActivity.this);
                     startActivity(i);
                 }
-            });
-        }
-        else{
-            Toast.makeText(this,"A timer is running",Toast.LENGTH_SHORT);
-        }
+                else{
+                    Toast.makeText(TimeoutActivity.this, "A timer is running",Toast.LENGTH_SHORT);
+                }
+            }
+        });
+
 
     }
 
-
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getDuration();
+    }
 
     private void stopTimer() {
         timer.cancel();
     }
 
     private void startTimer() {
-        timer= new CountDownTimer(timeLeft,1000) {
-            @Override
-            public void onTick(long l) {
-                updateTimer();
-                timeLeft--;
-            }
+        timer=new CountDownTimer(timeLeft*1000, 1000) {
 
-            @Override
+            public void onTick(long secondsLeft) {
+                timeLeft=((int)secondsLeft)/1000;
+                updateTimer((int)secondsLeft / 1000);
+            }
             public void onFinish() {
-                //notification()
+                timeoutText.setText("done!");
             }
         }.start();
     }
-
-    private void updateTimer() {
-        int minute=timeLeft/60;
-        int second=timeLeft%60;
-        String timerText=minute+":"+second;
-        timeout.setText(timerText);
+    private void updateTimer(int l) {
+        int minute=l/60;
+        int second=l%60;
+        timeoutText.setText(getString(R.string.timerTextFormat,minute,second));
     }
     public void getDuration(){
         initialTime = TimeoutOptionActivity.getDuration(this)*60;
-
+        Log.e("duration",initialTime+"");
+        timeLeft=initialTime;
     }
 
 }
