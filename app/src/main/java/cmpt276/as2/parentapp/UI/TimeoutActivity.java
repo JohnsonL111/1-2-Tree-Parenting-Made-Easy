c802cb1fd8eeaa669e35ae6a6f81e930b22b1dcd
jpa143 <jonathan_patrick@sfu.ca>
2021-11-01 23:03:45 -0700

    implement timer animation
    -add timer animation
    -fix bug where if the user only enter minute or second the app did not record
    -add pictures for background

--- app/src/main/java/cmpt276/as2/parentapp/UI/TimeoutActivity.java
+++ app/src/main/java/cmpt276/as2/parentapp/UI/TimeoutActivity.java
@@ -3,13 +3,18 @@ package cmpt276.as2.parentapp.UI;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,7 +29,7 @@ public class TimeoutActivity extends AppCompatActivity {
     private static final String DURATION_SETTING = "Duration Settings";
     private static final String DURATION_CHOICE = "Duration Choice";
 
-
+    Animation rotate;
     Button timerButton;
     Button resetButton;
     Button optionButton;
@@ -48,7 +53,7 @@ public class TimeoutActivity extends AppCompatActivity {
         resetButton = findViewById(R.id.ResetButton);
         optionButton=findViewById(R.id.OptionButton);
         timeoutText=findViewById(R.id.timeoutText);
-
+        timeoutText.setTextSize(40);
         timerButton.setText("START");
         timerButton.setOnClickListener(new View.OnClickListener() {
 
@@ -58,13 +63,15 @@ public class TimeoutActivity extends AppCompatActivity {
                     startTimer();
                     timerIsRunning=true;
                     timerButton.setText("STOP");
-
+                    optionButton.setAlpha(0);
 
                 }
                 else{
                     stopTimer();
                     timerIsRunning=false;
                     timerButton.setText("START");
+                    optionButton.setAlpha(1);
+
 
                 }
             }
@@ -75,8 +82,11 @@ public class TimeoutActivity extends AppCompatActivity {
             public void onClick(View view) {
                 timer.cancel();
                 timeLeft=initialTime;
+                timerIsRunning=false;
+                optionButton.setAlpha(1);
+                timerButton.setText("START");
                 updateTimer(timeLeft);
-                startTimer();
+
             }
         });
 
@@ -100,32 +110,75 @@ public class TimeoutActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         getDuration();
+        updateTimer(timeLeft);
     }
 
     private void stopTimer() {
+        rotate.cancel();
+        rotate.setFillAfter(true);
         timer.cancel();
     }
 
     private void startTimer() {
+        setTimerAnimation();
         timer=new CountDownTimer(timeLeft*1000, 1000) {
-
             public void onTick(long secondsLeft) {
                 timeLeft=((int)secondsLeft)/1000;
                 updateTimer((int)secondsLeft / 1000);
             }
             public void onFinish() {
                 timeoutText.setText("done!");
+                timerIsRunning=false;
+                timerButton.setText("START");
+                timeLeft=initialTime;
             }
-        }.start();
+        };
+        timer.start();
     }
+
+    private void setTimerAnimation() {
+        rotate = AnimationUtils.loadAnimation(TimeoutActivity.this,R.anim.rotate);
+        TableLayout animationLayout= findViewById(R.id.animationLayout);
+        ImageView timerTop = new ImageView(this)       ;
+        ImageView timerBottom = new ImageView(this)       ;
+        timerTop.setImageResource(R.drawable.round);
+
+        animationLayout.removeAllViewsInLayout();
+        animationLayout.addView(timerTop);
+        animationLayout.addView(timerBottom);
+
+        rotate.setDuration((long)timeLeft*1000);
+        timerTop.startAnimation(rotate);
+        rotate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                timerTop.setVisibility(View.VISIBLE);
+                timerTop.setImageAlpha(100);
+                timerBottom.setVisibility(View.VISIBLE);
+                timerBottom.setImageAlpha(100);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+
     private void updateTimer(int l) {
         int minute=l/60;
         int second=l%60;
         timeoutText.setText(getString(R.string.timerTextFormat,minute,second));
     }
     public void getDuration(){
-        initialTime = TimeoutOptionActivity.getDuration(this)*60;
-        Log.e("duration",initialTime+"");
+        initialTime = TimeoutOptionActivity.getDuration(this);
         timeLeft=initialTime;
     }
 
