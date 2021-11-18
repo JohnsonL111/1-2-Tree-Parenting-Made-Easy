01ed1481adaa59a84fb255d8f10afa80a93fb934
DanielZ <yza608@sfu.ca>
2021-11-18 14:32:25 -0800

    Refactor code in CoinFlipActivity

--- app/src/main/java/cmpt276/as2/parentapp/UI/CoinFlipActivity.java
+++ app/src/main/java/cmpt276/as2/parentapp/UI/CoinFlipActivity.java
@@ -5,8 +5,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.MediaPlayer;
-import android.widget.MediaController;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
